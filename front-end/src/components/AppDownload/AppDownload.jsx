3e9491f2970b1fcbeb6db0c1b45e95961b5ac02d
rupaliwakhare <you@example.com>
2025-10-14 16:04:00 +0530

    Restorent-project

---
+++ front-end/src/components/AppDownload/AppDownload.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import './AppDownload.css'
+
+const AppDownload = () => {
+  return (
+    <div className='App-Download' id='App-Download'>
+        <p>For Better Experience Download <br /> Tomato App</p>
+        div.app-app
+      
+    </div>
+  )
+}
+
+export default AppDownload
---
+++ front-end/src/context/StoreContext.jsx
@@ -0,0 +1,48 @@
+// context/StoreContext.js
+import { createContext, useEffect, useState } from "react";
+import { food_list } from "../assets/frontend_assets/assets";
+
+export const StoreContext = createContext(null);
+
+const StoreContextProvider = (props) => {
+  const [cartItems, setCartItems] = useState({});
+
+  const addToCart = (itemId) => {
+    setCartItems((prev) => ({
+      ...prev,
+      [itemId]: (prev[itemId] || 0) + 1,
+    }));
+  };
+
+  const removeFromCart = (itemId) => {
+    setCartItems((prev) => {
+      const updated = { ...prev };
+      if (updated[itemId] > 1) {
+        updated[itemId] -= 1;
+      } else {
+        delete updated[itemId];
+      }
+      return updated;
+    });
+  };
+
+  useEffect(() => {
+    console.log("🛒 Cart Updated:", cartItems);
+  }, [cartItems]);
+
+  const contextValue = {
+    food_list,
+    cartItems,
+    setCartItems,
+    addToCart,
+    removeFromCart,
+  };
+
+  return (
+    <StoreContext.Provider value={contextValue}>
+      {props.children}
+    </StoreContext.Provider>
+  );
+};
+
+export default StoreContextProvider;
