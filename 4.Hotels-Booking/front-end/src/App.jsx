056fdf14b46a073308031e9eaf2670a9a71c6013
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-11-13 19:28:04 +0530

    navbar

--- 4.Hotels-Booking/front-end/src/App.jsx
+++ 4.Hotels-Booking/front-end/src/App.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar/Navbar.jsx";
+import { useLocation } from "react-router-dom";
 
 const App = () => {
+
+const isOwnerPath = useLocation().pathname.includes("owner");
+
   return (
     <div>
-      <Navbar />
+      {!isOwnerPath && <Navbar />}
     </div>
   );
 };
