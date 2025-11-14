7c7baa12e1fe93bd5e72053c83f7374d2e005c6c
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-11-14 21:49:21 +0530

    Hero-update

--- 4.Hotels-Booking/front-end/src/App.jsx
+++ 4.Hotels-Booking/front-end/src/App.jsx
@@ -1,22 +1,19 @@
 import React from "react";
 import "./App.css";
 import Navbar from "./Components/Navbar.jsx";
-import Home from "./Pages/Home.jsx"
+import Home from "./Pages/Home.jsx";
 import { Route, Routes, useLocation } from "react-router-dom";
 
 const App = () => {
-
-const isOwnerPath = useLocation().pathname.includes("owner");
+  const isOwnerPath = useLocation().pathname.includes("owner");
 
   return (
     <div>
       {!isOwnerPath && <Navbar />}
       <div className="min-h-[70vh">
-  
-          <Routes>
-            <Route path="/" element={<Home />} />
-          </Routes>
-       
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
       </div>
     </div>
   );
--- 4.Hotels-Booking/front-end/src/Pages/Home.jsx
+++ 4.Hotels-Booking/front-end/src/Pages/Home.jsx
@@ -4,10 +4,12 @@ import Hero from '../Components/Hero.jsx'
 
 const Home = () => {
   return (
-    <div>
-      <Hero />
+   <>
+   <Hero />
+  </>
       
-    </div>
+      
+ 
   )
 }
 
