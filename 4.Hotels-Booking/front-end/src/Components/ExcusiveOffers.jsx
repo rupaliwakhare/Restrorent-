c37ca546d82657e13bcdc8326935ae8cab8353df
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-11-16 16:34:18 +0530

    exclusive offerse

---
+++ 4.Hotels-Booking/front-end/src/Components/ExcusiveOffers.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { assets, exclusiveOffers } from "../assets/assets";
+import Title from "./Title";
+
+const ExcusiveOffers = () => {
+  return (
+    <div className="flex flex-col items-center px-6 md:px-16 lg:px-24 lx:px-32 pt-20 pb-30">
+      <div className="flex flex-col md:flex-row items-center justify-between w-full">
+        <Title
+          align="left"
+          title="Eclusive Offers"
+          subTitle="Take advantage of our limited-time offers and special package to enhance your stay and create unforgettable memories."
+        />
+        <button className="group flex items-center gap-2 font-medium curser-pointer max-md:mt-12">
+          View All Offers
+          <img
+            src={assets.arrowIcon}
+            alt="arrow-icon"
+            className="group-hover:translate-x-1 transition-all"
+          />
+        </button>
+      </div>
+
+      <div>
+        {exclusiveOffers.map((item)=>(
+            <div key={item._id} className="group relative flex flex-col items-start justofy-between gap-1 pt-12 md:pt-18 px-4 rounded-xl text-white bg-no-repeat bg-cover bg-center" style={{backgroundImage:`url(${item.image})`}}>
+                <p className="px-3 py-1 absolute top-4 left-4 text-xs bg-white text-greay-800 font-medium rounded-full">{item.priceOff} % OFF</p>
+                <div >
+                    <p className="text-2xl font-medium font-playfair">{item.title}</p>
+                    <p>{item.description}</p>
+                    <p className="text-xs text-white/70 mt-3">Expires {item.expiryDate}</p>
+                </div>
+                <button className="flex item-center gap-2 font-medium cursur-pointer mt-4 mb-5">
+                    Viwe Offers
+                    <img className="invert group-hover:translate-x-1 transition-all " src={assets.arrowIcon} alt="arrow-icon" />
+                </button>
+                </div>
+
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default ExcusiveOffers;
--- 4.Hotels-Booking/front-end/src/Pages/Home.jsx
+++ 4.Hotels-Booking/front-end/src/Pages/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Hero from '../Components/Hero.jsx'
 import FeatureDestination from '../Components/FeatureDestination.jsx'
+import ExcusiveOffers from '../Components/ExcusiveOffers.jsx'
 
 
 const Home = () => {
@@ -8,6 +9,7 @@ const Home = () => {
    <>
    <Hero />
    <FeatureDestination />
+   <ExcusiveOffers />
   </>
       
       
