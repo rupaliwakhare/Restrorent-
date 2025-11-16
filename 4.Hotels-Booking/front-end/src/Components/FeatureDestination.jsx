abdcf67bd99c84cbba0932efe96107f4a00a89ed
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-11-16 15:56:10 +0530

    feature-destination

--- 4.Hotels-Booking/front-end/src/Components/FeatureDestination.jsx
+++ 4.Hotels-Booking/front-end/src/Components/FeatureDestination.jsx
@@ -1,17 +1,35 @@
 import React from 'react'
 import { roomsDummyData } from '../assets/assets.js'
 import HotelCard from './HotelCard'
+import Title from './Title.jsx'
+import { useNavigate } from 'react-router-dom'
 
 const FeatureDestination = () => {
+    const navigate = useNavigate()
   return (
-    <div className='flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20'>
-      <div className='flex flex-wrap items-center justify-center gap-6 mt-20'>
-        {roomsDummyData.slice(0,4).map((room,index)=>(
-            <HotelCard key={room._id} room={room} index={index}/>
+    <div className="flex flex-col items-center px-6 md:px-16 lg:px-24 bg-slate-50 py-20">
+      <Title
+        title="Feature Destination"
+        subTitle="Discover our handqicked selection of exceptional properties around the world, offering unparelleled luxury and unforgettable experience."
+      />
+      <div className="flex flex-wrap items-center justify-center gap-6 mt-20">
+        {roomsDummyData.slice(0, 4).map((room, index) => (
+          <div className="w-[250px]">
+            <HotelCard key={room._id} room={room} index={index} />
+          </div>
         ))}
       </div>
+      <button
+        onClick={() => {
+          navigate("/rooms");
+          scrollTo(0, 0);
+        }}
+        className="my-16 px-4 py2 text-sm font-medium border border-gray-300 rounded bg-white hover:bg-gray-50 transition-all cursur-pointer"
+      >
+        View All Destination
+      </button>
     </div>
-  )
+  );
 }
 
 export default FeatureDestination
