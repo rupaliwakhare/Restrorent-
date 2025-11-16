cfacf241ce16953dbf594cc6cb2baa6c8f628ffa
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-11-16 17:21:45 +0530

    exlusive-offers

--- 4.Hotels-Booking/front-end/src/Components/ExcusiveOffers.jsx
+++ 4.Hotels-Booking/front-end/src/Components/ExcusiveOffers.jsx
@@ -21,21 +21,32 @@ const ExcusiveOffers = () => {
         </button>
       </div>
 
-      <div>
-        {exclusiveOffers.map((item)=>(
-            <div key={item._id} className="group relative flex flex-col items-start justofy-between gap-1 pt-12 md:pt-18 px-4 rounded-xl text-white bg-no-repeat bg-cover bg-center" style={{backgroundImage:`url(${item.image})`}}>
-                <p className="px-3 py-1 absolute top-4 left-4 text-xs bg-white text-greay-800 font-medium rounded-full">{item.priceOff} % OFF</p>
-                <div >
-                    <p className="text-2xl font-medium font-playfair">{item.title}</p>
-                    <p>{item.description}</p>
-                    <p className="text-xs text-white/70 mt-3">Expires {item.expiryDate}</p>
-                </div>
-                <button className="flex item-center gap-2 font-medium cursur-pointer mt-4 mb-5">
-                    Viwe Offers
-                    <img className="invert group-hover:translate-x-1 transition-all " src={assets.arrowIcon} alt="arrow-icon" />
-                </button>
-                </div>
-
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 mt-12">
+        {exclusiveOffers.map((item) => (
+          <div
+            key={item._id}
+            className="group relative flex flex-col items-start justofy-between gap-1 pt-12 md:pt-18 px-4 rounded-xl text-white bg-no-repeat bg-cover bg-center"
+            style={{ backgroundImage: `url(${item.image})` }}
+          >
+            <p className="px-3 py-1 absolute top-4 left-4 text-xs bg-white text-gray-800 font-medium rounded-full">
+              {item.priceOff} % OFF
+            </p>
+            <div>
+              <p className="text-2xl font-medium font-playfair">{item.title}</p>
+              <p>{item.description}</p>
+              <p className="text-xs text-white/70 mt-3">
+                Expires {item.expiryDate}
+              </p>
+            </div>
+            <button className="flex item-center gap-2 font-medium cursur-pointer mt-4 mb-5">
+              Viwe Offers
+              <img
+                className="invert group-hover:translate-x-1 transition-all "
+                src={assets.arrowIcon}
+                alt="arrow-icon"
+              />
+            </button>
+          </div>
         ))}
       </div>
     </div>
