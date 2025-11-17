ef6a532d33332ba4178ad9b4ebb594e55e5e46d3
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-11-17 22:03:36 +0530

    Footer

--- 4.Hotels-Booking/front-end/src/App.jsx
+++ 4.Hotels-Booking/front-end/src/App.jsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Navbar from "./Components/Navbar.jsx";
 import Home from "./Pages/Home.jsx";
 import { Route, Routes, useLocation } from "react-router-dom";
+import Footer from "./Components/Footer.jsx";
 
 const App = () => {
   const isOwnerPath = useLocation().pathname.includes("owner");
@@ -15,6 +16,7 @@ const App = () => {
           <Route path="/" element={<Home />} />
         </Routes>
       </div>
+      <Footer />
     </div>
   );
 };
