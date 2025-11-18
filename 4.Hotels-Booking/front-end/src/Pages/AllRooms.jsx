dfb59f4af4985fde8e37b2de8a558efaeb19307c
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-11-18 15:04:20 +0530

    hotles-details-page

--- 4.Hotels-Booking/front-end/src/Pages/AllRooms.jsx
+++ 4.Hotels-Booking/front-end/src/Pages/AllRooms.jsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { assets, facilityIcons, roomsDummyData } from "../assets/assets";
 import { useNavigate } from "react-router-dom";
 import StarRating from "../Components/StarRating";
 
 const AllRooms = () => {
   const navigate = useNavigate();
+  const [openFilters, setOpenFilters] = useState(false)
 
   return (
     <div className="flex flex-col-reverse lg:flex-row items-start justify-between pt-28 md:pt-35 px-4 md:px-16 lg:px-24">
       <div className="w-full">
         <div className="flex flex-col items-start text-left mb-8">
           <h1 className="text-4xl md:text-[40px] font-playfair">Hotel Rooms</h1>
-          <p className="text-sm md:text-base text-gray-500/90 mt-2">
+          <p className=" w-[80%] text-sm md:text-base text-gray-500/90 mt-2">
             Take advantage of our limited-time offers and special packages to
             enhance your stay and create unforgettable memories.
           </p>
@@ -22,7 +23,6 @@ const AllRooms = () => {
             key={room._id}
             className="flex flex-col md:flex-row items-start py-10 gap-6 border-b border-gray-300 last:pb-30 last:border-0"
           >
-           
             <div className="h-64 w-full md:w-1/2 overflow-hidden rounded-xl">
               <img
                 onClick={() => {
@@ -36,7 +36,6 @@ const AllRooms = () => {
               />
             </div>
 
-          
             <div className="md:w-1/2 flex flex-col gap-2">
               <p className="text-gray-500">{room.hotel.city}</p>
               <p
@@ -58,26 +57,37 @@ const AllRooms = () => {
               </div>
               {/* Room Amenities */}
               <div className="flex flex-wrap items-center mt-3 mb-6 gap-4">
-                {room.amenities.map((item,index)=>(
-                  <div key={index} className="flex items-center gap-2 px-3 py-2 rounded-lg bg-[#f5f5ff]/70">
-                    <img src={facilityIcons[item] } alt={item} className="w-5 h-5"/>
+                {room.amenities.map((item, index) => (
+                  <div
+                    key={index}
+                    className="flex items-center gap-2 px-3 py-2 rounded-lg bg-[#f5f5ff]/70"
+                  >
+                    <img
+                      src={facilityIcons[item]}
+                      alt={item}
+                      className="w-5 h-5"
+                    />
                     <p className="text-xs">{item}</p>
                   </div>
                 ))}
-                {/* Room Price per Night */}
-                <p className="text-xl font-medium text-gray-700">${room.pricePerNight}/night</p>
               </div>
+
+              {/* Room Price per Night */}
+              <p className="text-xl font-medium text-gray-700">
+                ${room.pricePerNight}/night
+              </p>
             </div>
           </div>
         ))}
       </div>
 
       {/* Filters Section */}
-      <div className="bg-white w-80 border-gray-300 text-gray-600 max-lg:mb-8 min-lg:mt-16">
-        <div>
-          <p> FILTERS</p>
-          <div>
-            <span>CLEAR</span>
+      <div className="bg-white w-80 border border-gray-300 text-gray-600 max-lg:mb-8 lg:mt-16">
+        <div className={`flex items-center justify-between px-5 py-2.5 lg:border-b border-gray-300 ${openFilters && "border-b"}`}>
+          <p className="text-base font-medium text-gray-800">FILTERS</p>
+          <div className="text-xs cursor-pointer">
+            <span onClick={()=>setOpenFilters(!openFilters)} className="lg:hidden">{openFilters ? 'HIDE' : "SHOW"}HIDE</span>
+            <span className="hidden lg:block">CLEAR</span>
           </div>
         </div>
       </div>
