95df9c451208336ecb7528ddae2a564af2858efd
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-11-29 20:45:40 +0530

    List-Rooms

--- 4.Hotels-Booking/front-end/src/Pages/hotelOwner/ListRoom.jsx
+++ 4.Hotels-Booking/front-end/src/Pages/hotelOwner/ListRoom.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { roomsDummyData } from '../../assets/assets'
+import Title from '../../Components/Title'
 
 const ListRoom = () => {
+
+
+const [rooms,setRooms] = useState(roomsDummyData)
+
   return (
     <div>
-      
+      <Title align='left' font='outfit' title="Room Listings" subTitle='View , edit or monage all listed rooms. keep the information up-to-date to provide the best experience for users.'/>
     </div>
   )
 }
