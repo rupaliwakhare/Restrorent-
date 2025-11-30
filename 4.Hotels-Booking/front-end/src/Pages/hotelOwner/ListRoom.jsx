f8d1b98129d491248df804df67a20e020b8f2d88
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-11-30 14:36:35 +0530

    List-Rooms

--- 4.Hotels-Booking/front-end/src/Pages/hotelOwner/ListRoom.jsx
+++ 4.Hotels-Booking/front-end/src/Pages/hotelOwner/ListRoom.jsx
@@ -9,9 +9,59 @@ const [rooms,setRooms] = useState(roomsDummyData)
 
   return (
     <div>
-      <Title align='left' font='outfit' title="Room Listings" subTitle='View , edit or monage all listed rooms. keep the information up-to-date to provide the best experience for users.'/>
+      <Title
+        align="left"
+        font="outfit"
+        title="Room Listings"
+        subTitle="View , edit or monage all listed rooms. keep the information up-to-date to provide the best experience for users."
+      />
+      <p className="text-gray-500 mt-8">All Rooms</p>
+      <div className="w-full max-w-3xl text-left border border-gray-300 rounded-lg max-h-80 overflow-scroll mt-3">
+        <table className="w-full">
+          <thead className="bg-gray-50">
+            <tr>
+              <th className="py-3 px-4 text-gray-800 font-medium"> Name</th>
+              <th className="py-3 px-4 text-gray-800 font-medium max-sm:hidden">
+                Facility
+              </th>
+              <th className="py-3 px-4 text-gray-800 font-medium text-center">
+                Price / night
+              </th>
+              <th className="py-3 px-4 text-gray-800 font-medium text-center">
+                Action
+              </th>
+            </tr>
+          </thead>
+          <tbody className="text-sm">
+            {rooms.map((item, index) => (
+              <tr key={index}>
+                <td className="py-3 px-4 text-gray-700 border-t border-gray-300">
+                  {item.roomType}
+                </td>
+                <td className="py-3 px-4 text-gray-700 border-t border-gray-300 max-sm:hidden">
+                  {item.amenities.join(", ")}
+                </td>
+                <td className="py-3 px-4 text-gray-700 border-t border-gray-300 ">
+                  {item.pricePerNight}
+                </td>
+                <td className="py-3 px-4 border-t border-gray-300 text-sm text-red-500 text-center">
+                  <label className="relative inline-flex items-center cursor-pointer text-gray-900 gap-3">
+                    <input
+                      type="checkbox"
+                      className="sr-only peer"
+                      checked={item.isAvailable}
+                    />
+                    <div className="w-12 h-6 bg-slate-600 rounded-full peer-checked:bg-blue-500 transition-colors duration-200"></div>
+                    <span className="dot absolute left-1 top-1 w-5 h-5 bg-white rounded-full transition-transform duration-200 ease-in-out peer-checked:translate-x-5"></span>
+                  </label>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
-  )
+  );
 }
 
 export default ListRoom
