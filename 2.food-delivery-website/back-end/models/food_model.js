c73ef1ea43231c01157823e5f019dc8a97435330
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-12-03 18:52:47 +0530

    food

--- 2.food-delivery-website/back-end/models/food_model.js
+++ 2.food-delivery-website/back-end/models/food_model.js
@@ -3,11 +3,12 @@ import mongoose from "mongoose";
 const foodSchema = new mongoose.Schema({
     name:{type:String,required:true},
     description:{type:String,required:true},
-    price:{type:Number,required:true},
+    price:{type:String,required:true},
     image:{type:String,required:true},
     category:{type:String,required:true}
 })
 
+mongoose.models = {}
 const foodModel = mongoose.models.food || mongoose.model("food",foodSchema)
 
 export default foodModel;
