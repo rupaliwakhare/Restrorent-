7efa71e051dbf723ed396b3624572c74f4cdd72b
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-12-04 15:01:44 +0530

    admin-navbar

--- 2.food-delivery-website/admin/src/App.jsx
+++ 2.food-delivery-website/admin/src/App.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
+import Navbar from './components/Navbar/Navbar.jsx'
+import Sidebar from './components/Sidebar/Sidebar.jsx'
 
 const App = () => {
   return (
     <div>
-      
+      <Navbar />
+      <div className="app-content">
+        <Sidebar />
+      </div>
     </div>
   )
 }
--- 2.food-delivery-website/admin/src/components/Navbar/Navbar.jsx
+++ 2.food-delivery-website/admin/src/components/Navbar/Navbar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import './Navbar.css'
+import { assets } from '../../assets/assets'
 const Navbar = () => {
   return (
-    <div>
+    <div className='navbar'>
+       <img className='logo' src={assets.logo} alt="" />
+       <img src={assets.profile_image} alt="" className="profile-icon" />
+       
       
     </div>
   )
