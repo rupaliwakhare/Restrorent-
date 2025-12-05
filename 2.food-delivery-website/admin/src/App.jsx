a0789f7af082c81cf2510f2a73bb59aedc7758c7
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-12-05 12:02:10 +0530

    add-page

--- 2.food-delivery-website/admin/src/App.jsx
+++ 2.food-delivery-website/admin/src/App.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import Navbar from './components/Navbar/Navbar.jsx'
 import Sidebar from './components/Sidebar/Sidebar.jsx'
+import { BrowserRouter as Router, Routes,Route } from 'react-router-dom'
+import List from './pages/List/List.jsx'
+import Orders from './pages/Orders/Orders.jsx'
+import Add from './pages/Add/Add.jsx'
 
 const App = () => {
   return (
@@ -9,6 +13,11 @@ const App = () => {
       <hr />
       <div className="app-content">
         <Sidebar/>
+        <Routes>
+          <Route path='/add' element={<Add/>}/>
+          <Route path='/list' element={<List/>}/>
+          <Route path='/orders' element={<Orders/>}/>
+        </Routes>
       </div>
     </div>
   )
--- 2.food-delivery-website/admin/src/pages/Add/Add.jsx
+++ 2.food-delivery-website/admin/src/pages/Add/Add.jsx
@@ -1,11 +1,94 @@
-import React from 'react'
-import './Add.css'
+import React, { useState } from "react";
+import "./Add.css";
+import { assets } from "../../assets/assets";
 const Add = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+    const [image,setImage] = useState(false)
+    const [data,setData] =useState({
+        name:"",
+        description:"",
+        price:"",
+        category:"Salad"
+    })
+
+const onChangeHandler = (event)=>{
+    const name = event.target.name;
+    const value = event.target.value;
+    setData(data=>({...data,[name]:value}))
 }
 
-export default Add
+
+
+  return (
+    <div className="add">
+      <form className="flex-col">
+        <div className="add-img-upload flex-col">
+          <p>Upload Image</p>
+          <label htmlFor="image">
+            <img
+              src={image ? URL.createObjectURL(image) : assets.upload_area}
+              alt=""
+            />
+          </label>
+          <input
+            onChange={(e) => setImage(e.target.files[0])}
+            type="file"
+            id="image"
+            hidden
+            required
+          />
+        </div>
+        <div className="add-product-name flex-col">
+          <p>Product Name</p>
+          <input
+            onChange={onChangeHandler}
+            value={data.name}
+            type="text"
+            name="name"
+            placeholder="Type here"
+          />
+        </div>
+        <div className="add-product-description flex-col">
+          <p>Product description</p>
+          <textarea
+            onChange={onChangeHandler}
+            value={data.description}
+            name="description"
+            rows="6"
+            placeholder="Write content here"
+          ></textarea>
+        </div>
+        <div className="add-category-price">
+          <div className="add-category flex-col">
+            <p>Product category</p>
+            <select onChange={onChangeHandler} name="category">
+              <option value="Salad">Salad</option>
+              <option value="Rolls">Rolls</option>
+              <option value="Dessert">Dessert</option>
+              <option value="Sandwich">SAndwich</option>
+              <option value="Cake">Cake</option>
+              <option value="Pure Veg">Pure Veg</option>
+              <option value="Pasta">Pasta</option>
+              <option value="Noodles">Noodles</option>
+            </select>
+          </div>
+          <div className="add-price flex-col">
+            <p>Product price</p>
+            <input
+              onChange={onChangeHandler}
+              value={data.price}
+              type="number"
+              name="price"
+              placeholder="₹20"
+            />
+          </div>
+        </div>
+        <button type="submit" className="add-btn">
+          Add
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default Add;
