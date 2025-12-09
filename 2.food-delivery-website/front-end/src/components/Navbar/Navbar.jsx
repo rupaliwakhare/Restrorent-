cbfe015ccb0d9246bcae264ee1128a67c9987210
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-12-09 15:15:33 +0530

    navbar

--- 2.food-delivery-website/front-end/src/components/Navbar/Navbar.jsx
+++ 2.food-delivery-website/front-end/src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ import { StoreContext } from '../../context/StoreContext'
 const Navbar = ({setShowLogin}) => {
 
     const[menu,setMenu] = useState("home");
-    const {getTotalCartAmount} = useContext(StoreContext);
+    const {getTotalCartAmount,token,setToken} = useContext(StoreContext);
 
   return (
     <div className="navbar">
@@ -52,7 +52,16 @@ const Navbar = ({setShowLogin}) => {
           </Link>
           <div className={getTotalCartAmount() === 0 ? "" : "dot"}></div>
         </div>
-        <button onClick={() => setShowLogin(true)}>Sign In</button>
+        {!token ? <button onClick={() => setShowLogin(true)}>Sign In</button>:
+        <div className='navbar-profile'>
+          <img src={assets.profile_icon} alt="" />
+          <ul className="nav-profile-dropdown">
+            <li><img src={assets.bag_icon} alt="" /><p>Orders</p></li>
+            <hr />
+            <li><img src={assets.logout_icon} alt="" />Logout</li>
+          </ul>
+          </div>}
+        
       </div>
     </div>
   );
