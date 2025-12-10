7d9eea51b704271d4d60444276f8328adcdaebc6
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-12-10 21:18:01 +0530

    room

---
+++ 4.Hotels-Booking/back-end/controllers/roomController.js
@@ -0,0 +1,27 @@
+// API to create a new room for a hotel
+
+export const createRoom = async (req,res)=>{
+
+}
+
+
+// API to get a all rooms for a hotel
+
+export const getRooms = async (req,res)=>{
+
+}
+
+
+
+// API to get a all rooms for a specific hotel
+
+export const getOwnerRooms = async (req,res)=>{
+
+}
+
+
+// API to toggle availability of a room
+
+export const toggleRoomAvailability = async (req,res)=>{
+
+}
