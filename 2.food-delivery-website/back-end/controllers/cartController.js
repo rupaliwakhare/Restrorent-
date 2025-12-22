e3bc26ca346a2e078a53ba980e242d530b1daaba
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-12-22 15:17:33 +0530

    cart-functionality

--- 2.food-delivery-website/back-end/controllers/cartController.js
+++ 2.food-delivery-website/back-end/controllers/cartController.js
@@ -1,24 +1,30 @@
-import userModel from "../models/user_model.js"
+import userModel from "../models/user_model.js";
 
 // add items to usercart
 
-const addToCart = async (req,res)=>{
-    try {
-        let userData = await userModel.findOne({_id:req.body.userID})
-    } catch (error) {
-        
+const addToCart = async (req, res) => {
+  try {
+    let userData = await userModel.findOne({ _id: req.body.userID });
+    let cartData = await userData.cartData;
+    if (!cartData[req.body.itemId]) {
+      cartData[req.body.itemId] = 1;
+    }else{
+        cartData[req.body.itemId] += 1
     }
-}
+    await userModel.findByIdAndUpdate(req.body.userId,{cartData});
+    res.json({success:true,message:"Added To Cart"})
+  } catch (error) {
+    console.log(error);
+    res.json({success:false,message:"Error"})
+  }
+};
 
 // remove items from user cart
 
-const removeFromCart = async(req,res)=>{
+const removeFromCart = async (req, res) => {};
 
-}
+const getCart = async (req, res) => {
+  // fetch user cart data
+};
 
-// fetch user cart data
-const getCart = async(req,res)=>{
-
-}
-
-export {addToCart,removeFromCart,getCart}
+export { addToCart, removeFromCart, getCart };
