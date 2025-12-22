602a1a71210b433ddd70edd42dfc107d07fa7127
rupaliwakhare <rupaliwakhare184@gmail.com>
2025-12-22 15:07:51 +0530

    cart-controller

--- 2.food-delivery-website/back-end/controllers/cartController.js
+++ 2.food-delivery-website/back-end/controllers/cartController.js
@@ -3,7 +3,11 @@ import userModel from "../models/user_model.js"
 // add items to usercart
 
 const addToCart = async (req,res)=>{
-
+    try {
+        let userData = await userModel.findOne({_id:req.body.userID})
+    } catch (error) {
+        
+    }
 }
 
 // remove items from user cart
